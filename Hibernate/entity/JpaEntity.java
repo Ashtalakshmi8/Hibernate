8437a8289bdf25eaa9965ee621a85791547f7f32
Ashtalakshmi8 <96108533+Ashtalakshmi8@users.noreply.github.com>
2022-04-27 15:23:04 +0530

    Add files via upload
    Task

---
+++ Hibernate/entity/JpaEntity.java
@@ -0,0 +1,30 @@
+package com.xworkz.hibernatedemoapp.entity;
+
+
+	
+
+	import jakarta.persistence.Column;
+	import jakarta.persistence.Entity;
+	import jakarta.persistence.Id;
+	import jakarta.persistence.Table;
+	import lombok.Data;
+
+	@Data
+	@Entity
+	@Table(name = "jpa")
+
+	public class JpaEntity { 
+
+		@Id
+		@Column(name = "id")
+		private int ID;
+		//@Column(name = "contact_no")
+		private long contactNo;
+		private String name;
+		private String type;
+		private double rating;
+	}
+
+	
+	
+
